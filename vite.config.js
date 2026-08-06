633d5dbaf1434788580afa33206148a37b7bddbf
rhythmhere <rhythmbhattari90@gmail.com>
2026-08-06 09:18:43 +0545

    commit vite.config.js individually

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': process.env.VITE_API_PROXY_TARGET || 'http://127.0.0.1:8000'
+    }
+  }
+});
